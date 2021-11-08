4e59e09d06d75b7a1ed0e830f6023818e1574ab1
sonakshi <sonakshi.jai92@gmail.com>
2021-11-08 20:04:46 +0530

    added to cart

--- cart.php
+++ cart.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
+echo "<pre>";
+print_r($_SESSION);
+//session_destroy();
 require_once("header.php");
 require_once("dbconn.php");
+$total = $gTotal = 0;
 ?>
 <h1 class="txt-heading">Products</h1>
 <button class="btn btn-warning">Clear Cart</button>
@@ -16,20 +21,62 @@ require_once("dbconn.php");
 	</tr>
 	</thead>
 	<?php
+	if(empty($_POST['prod_id'])){
+		$_POST['prod_id']="";
+		$_POST['qnty']= "";
+	}
     extract($_POST);
-	$product = mysqli_query($conn, "SELECT * FROM products WHERE id=$id");
-	if (!empty($product)) { 
-		while($product_array= mysqli_fetch_array($product)){
+	$id = $_POST['prod_id'];
+	$qnty = $_POST['qnty'];
+	if($id != ''){
+		if(!empty($qnty)) {
+			$query = mysqli_query($conn, "SELECT * FROM products WHERE id=$id");
+			$productByCode = mysqli_fetch_assoc($query);
+			// print_r($productByCode);
+			// echo $productByCode["id"];
+			// exit();
+			$itemArray = array('id'=>$productByCode["id"],'name'=>$productByCode["name"], 'quantity'=>$qnty, 'price'=>$productByCode["price"],'image'=>$productByCode["image"] );
+			
+			$x =in_array($productByCode["id"],$_SESSION["cart_item"],TRUE);
+			var_dump($x);
+			if(!empty($_SESSION["cart_item"])) {
+				if(array_key_exists($productByCode["id"],$_SESSION["cart_item"])) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode["id"] == $k)
+								$_SESSION["cart_item"][$k]["quantity"] = $_POST["qnty"];
+					}
+				} else {
+					
+					$_SESSION["cart_item"][$productByCode["id"]] = $itemArray;
+				}
+			} else {
+				$_SESSION["cart_item"][$productByCode["id"]] = $itemArray; // created session 1st time whensite is open
+			}
+		}
+	}
+
+	
+	//$id = $_SESSION['id'];
+
+
+	
+	if (!empty($_SESSION["cart_item"])) { 
+		foreach($_SESSION["cart_item"] as $key => $product_array){
 	?>
 	<tr>
 	<td><?php echo $product_array["name"]; ?></td>
 	<td><?php echo " $ ".$product_array["price"]; ?></td>
 	<td>
-		<button class="btn btn-sm btn-danger minus">-</button><span>
-		<input type="text"  class="" name="" id="qty" value="1" size="2"> <button class="btn btn-sm btn-success">+</button></span>
+		<button class="btn btn-sm btn-danger sub" >-</button>
+
+		<span><input type="text"  class="" name="" id="qty_<?php echo $product_array["id"]; ?>" value="<?php echo $product_array["quantity"];?>" size="2" >
+
+		<button class="btn btn-sm btn-success" onClick="increment_quantity(<?php echo $product_array["id"]; ?>)">+</button></span>
 	</td>
-	<td><?php echo " $ ".$product_array["price"]; ?></td>
+	<?php $total = $product_array["price"]*$product_array["quantity"];?>
+	<td><?php echo " $ ". $total ?></td>
 	<td><button class="btn btn-sm btn-danger"> X</button><span></td>
+	<?php $gTotal += $total ?>
 	</tr>
 	</div>
 	
@@ -37,13 +84,25 @@ require_once("dbconn.php");
 			}
 	}
 	?>
+	<tr>
+		<?php //$total .= $total;?>
+		<td><h3>Cart Total: $ </h3></td> 
+		<td><h3><?php echo $gTotal?> </h3></td> 
+	</tr>
+
 	</table>
 <br><br><br>
-	<h3>Cart Total: $ </h3> 
-	<input type="button" id="checkout"  value="Checkout"></td>
+	<input type="button" class="btn btn btn-success" id="checkout"  value="Checkout"></td>
 	</form>
 
 <script>
+function increment_quantity(cart_id) {
+    var newQuantity = parseInt($("#qty_"+cart_id).val())+1;
+    //$("#qty_"+cart_id).val(newQuantity);
+	//$_SESSION["cart_item"][$k]["quantity"] = newQuantity;
+	sessionStorage.setItem("['cart_item']['quantity']", "newQuantity");
+}
+
 	
 </script>
 </body>
--- products.php
+++ products.php
@@ -5,14 +5,18 @@ $product = mysqli_query($conn, "SELECT * FROM products");
 if (!empty($product)) { 
     while($product_array= mysqli_fetch_array($product)){
 ?>
+<script>
+    
+</script>
 <form id="product">
 <div class="border" id="display">
-    <input type="hidden" name="id" value="<?php echo $product_array["id"]; ?>">
+    <input type="hidden" name="id" id="prod_id" value="<?php echo $product_array["id"]; ?>">
     <div><img src="<?php echo $product_array["image"]; ?>" width="100" height="100"></div>
     <div><strong><?php echo $product_array["name"]; ?></strong></div>
     <div class="text-danger">Price:<?php echo " $ ".$product_array["price"]; ?></div>
-    <div><input type="add" name="add" id="add" class="btn btn-success addtocart" onClick="cartAction('add','<?php echo $product_array['id']; ?>')" value="Add to cart"></div>
-    <div><input type="button" name="added" id="added" style="display:none" class="addedtocart"  value="Already Added"></div>
+    <div><input type="button" name="add" id="add_<?php echo $product_array['id'];?>" onClick="cart(<?php echo $product_array['id'];?>);" class="btn btn-success addtocart" value="Add to cart"></div>
+    <div><input type="button" name="added" id="added_<?php echo $product_array['id'];?>" style="display:none" class="addedtocart"  value="Already Added"></div>
+    <input type="hidden" name="qnty" id="qnty" value="1">
 </form>
 </div >
 <?php } } else{?>
@@ -20,19 +24,24 @@ if (!empty($product)) {
 <?php } ?>
 
 <script>
-    $(document).ready(function(){
-		$('#add').click(function(){
-        alert("added");
+    //$(document).ready(function(){
+		//$('#add').click(function(){
+            function cart(id){
+            //var id = $('#prod_id').val();
+           // alert(id); return false;
+           var qnty=$('#qnty').val();
             jQuery.ajax({
                 url: 'cart.php',
-                data:id,
+                data:{prod_id:id, qnty:qnty},
                 type:'POST',
                 success:function(data){
-                    $("#cart").html(data);
-                        $("#add").hide();
-                        $("#addedtocart").show();
+                   //$("#cart").html(data);
+                    $("#add_"+id).hide();
+                    $("#added_"+id).show();
                 }
             });
-		});
-    })
+           
+		}
+           
+ //   });
 </script>
